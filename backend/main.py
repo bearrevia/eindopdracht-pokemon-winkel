17660d24b37e5d45598f19a76534b31002384878
dylan de beer <dylan.2001@live.nl>
2026-01-26 11:30:53 +0100

    initial commit and setup

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # pas aan als je andere frontend-poort gebruikt
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello from FastAPI"}
