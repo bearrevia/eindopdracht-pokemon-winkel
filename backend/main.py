956397f0f99793ae104b3399b2d2fdcab301e55b
dylan de beer <dylan.2001@live.nl>
2026-01-26 15:11:55 +0100

    database_connection

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from api.api import router as api_router
 
 app = FastAPI()
 
@@ -10,6 +11,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Include API router
+app.include_router(api_router, prefix="/api")
 
 @app.get("/")
 async def read_root():
